bc8f34ec46e96246fb67b438062e4c354daf88ad
Valentin Gurmeza <valentin.gurmeza@globaleagle.com>
2017-12-13 17:20:16 -0800

    just cleanup

--- lib/minasu/db.py
+++ lib/minasu/db.py
@@ -1,22 +1,23 @@
-import os,shutil
 from minasu import settings
+import os,shutil
+
 
 class db():
     def __init__(
         self,
-        instance_name, 
+        instance_name,
         instance_dir=settings.default_settings["instance_dir"]
     ):
         self.instance_name = instance_name
         self.instance_dir  = instance_dir
         self.instance_path = self.instance_dir + "/" + self.instance_name
 
-    ## create a new db at folder location
+    # create a new db at folder location
     def create(self):
         data = {}
-        #data["instance_name"] = self.instance_name
-        #data["instance_dir"]  = self.instance_dir + "/"
-        #data["instance_path"] = self.instance_path + "/"
+        # data["instance_name"] = self.instance_name
+        # data["instance_dir"]  = self.instance_dir + "/"
+        # data["instance_path"] = self.instance_path + "/"
 
         if not os.path.exists(self.instance_path):
             try:
@@ -31,7 +32,7 @@ class db():
 
         return data
 
-    ## recursively remove a directory and all of its contents
+    # recursively remove a directory and all of its contents
     def destroy(self):
         data = {}
 
@@ -46,4 +47,3 @@ class db():
     ## get group
 
     ## get host
-
