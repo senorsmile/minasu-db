e68254d98e7460865f45b45ce40b7ef300f0db57
Shaun Smiley <senorsmile@gmail.com>
2018-05-09 22:12:38 -0700

    get tox & pipenv working and add py35 to tox tests

--- run_tests.sh
+++ run_tests.sh
@@ -1,4 +1,8 @@
 #!/usr/bin/env bash
 
 #TODO: is there a way to make tox run pipenv?
+#pip list --format=legacy | grep pipenv >/dev/null || {
+#  pip install pipenv
+#}
+
 pipenv run python tests/test_instance.py
