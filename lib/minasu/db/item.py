df9ceda5250e1231f389f13e8a7ad3191ab7cfe9
Valentin Gurmeza <valentin.gurmeza@globaleagle.com>
2018-05-12 15:53:07 -0700

    add edit and delete function

--- lib/minasu/db/item.py
+++ lib/minasu/db/item.py
@@ -1,17 +1,35 @@
-import bucket
+import os
 
 
 class bucket_item():
-    ''' bucket_list exposes operation interfaces to the items in the bucket
+    ''' bucket_item exposes operation interfaces to the items in the bucket
     '''
-    def __init__(
-         self,
-         bucket_name
-    ):
-        self.bucket_name = bucket_name
-        self.bucket = bucket.load(bucket_name) # bucket.load() is not working yet
+    # not sure we need this yet
+    # def __init__(
+    #      self,
+    #      bucket_name,
+    #      bucket_path
+    # ):
+    #     self.bucket_name = bucket_name
+    #     self.bucket_path = bucket_path
 
-    def list(self):
+    def list(bucket_path):
         ''' list all the items in the bucket
         '''
-        return [x for x in os.listdir(self.buckets)]
+        return [x for x in os.listdir(self.bucket_path)]
+
+    def edit(bucket_path, item, content=0):
+        ''' create or edit an item and its content
+        '''
+        f = open(item + ".yml", mode='a')
+        if content != 0:
+            f.write(content)
+        else:
+            f.write('---')
+        f.close()
+
+    def delete(bucket_path item):
+        ''' delete item in the bucket
+        '''
+        os.remove(self.bucket_path + "/" + item)
+        return True
